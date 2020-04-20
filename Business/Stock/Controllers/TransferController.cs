5b29a56e207daa379629a234b7863c6473b78b6e
rick <huike2011@126.com>
2020-04-20 21:56:36 +0800

    初始化实体仓调拨模块

---
+++ Business/Stock/Controllers/TransferController.cs
@@ -0,0 +1,26 @@
+using System.Web.Http;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_wms_ef.Stock.Controllers
+{
+    
+    [Route("/api/stock/transfer/")]
+    [EnableCors("any")]
+    public class TransferController:ApiController
+    {
+        [Route("list")]
+        [HttpGet]
+        public JsonResult List()
+        {
+            return new JsonResult(true);
+        }
+
+        [Route("{id}/details")]
+        [HttpGet]
+        public JsonResult Details([FromUri] long id)
+        {
+            return new JsonResult(true);
+        }
+    }
+}

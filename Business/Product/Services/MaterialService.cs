081fe8c9e6f3f58941c08d5d5e38bd112f61b4b6
rick <rickli@to-lead.com.cn>
2020-05-29 18:35:44 +0800

    修改物料服务

--- Business/Product/Services/MaterialService.cs
+++ Business/Product/Services/MaterialService.cs
@@ -8,13 +8,13 @@ namespace dotnet_wms_ef.Product.Services
 {
     public class MaterialService
     {
-         wmsproductContext wmsproduct = new wmsproductContext();
+        wmsproductContext wmsproduct = new wmsproductContext();
         public TProdMat GetMatByBarcode(string barcode)
         {
             return wmsproduct.TProdMats.Where(x => x.Barcode == barcode).FirstOrDefault();
         }
 
-         public List<TProdMat> PageList(int page)
+        public List<TProdMat> PageList(int page)
         {
             return wmsproduct.TProdMats.ToList();
         }
@@ -26,19 +26,20 @@ namespace dotnet_wms_ef.Product.Services
 
         public bool Create(VMatAddForm vMat)
         {
-            TProdMat mat = new TProdMat{
-               Code = vMat.Code,
-               Name = vMat.Name,
-               Barcode = vMat.Barcode,
-               PUom = vMat.PUom,
-               AUom = vMat.AUom,
-               PToA = vMat.PToA,
-               X = vMat.X,
-               XUnit = vMat.XUnit,
-               Y = vMat.Y,
-               YUnit = vMat.YUnit,
-               Z = vMat.Z,
-               ZUnit = vMat.ZUnit,
+            TProdMat mat = new TProdMat
+            {
+                Code = vMat.Code,
+                Name = vMat.Name,
+                Barcode = vMat.Barcode,
+                PUom = vMat.PUom,
+                AUom = vMat.AUom,
+                PToA = vMat.PToA,
+                X = vMat.X,
+                XUnit = vMat.XUnit,
+                Y = vMat.Y,
+                YUnit = vMat.YUnit,
+                Z = vMat.Z,
+                ZUnit = vMat.ZUnit,
             };
 
             mat.CreatedBy = DefaultUser.UserName;
@@ -46,7 +47,7 @@ namespace dotnet_wms_ef.Product.Services
 
             wmsproduct.TProdMats.Add(mat);
 
-            return wmsproduct.SaveChanges()>0;
+            return wmsproduct.SaveChanges() > 0;
         }
 
     }
